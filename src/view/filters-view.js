a7c22dea3a9015763a7bc839e2408532d00ceb1c
IDDQD93 <ggteamsasha@gmail.com>
2025-04-07 17:52:57 +0000

    Добавление моделей данных: Destination, Offer, Point

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,24 +1,28 @@
 export default class FiltersView {
-    getTemplate() {
-      return `
-        <form class="trip-filters">
-          <div class="trip-filters__filter">
-            <input id="filter-everything" type="radio" name="trip-filter" checked>
-            <label for="filter-everything">Everything</label>
-          </div>
-          <div class="trip-filters__filter">
-            <input id="filter-future" type="radio" name="trip-filter">
-            <label for="filter-future">Future</label>
-          </div>
-          <div class="trip-filters__filter">
-            <input id="filter-past" type="radio" name="trip-filter">
-            <label for="filter-past">Past</label>
-          </div>
-        </form>
-      `;
-    }
-  
-    render(container) {
-      container.insertAdjacentHTML('beforeend', this.getTemplate());
-    }
+  getTemplate() {
+    return `
+      <form class="trip-filters">
+        <div class="trip-filters__filter">
+          <input id="filter-everything" type="radio" name="trip-filter" checked>
+          <label for="filter-everything">Everything</label>
+        </div>
+        <div class="trip-filters__filter">
+          <input id="filter-future" type="radio" name="trip-filter">
+          <label for="filter-future">Future</label>
+        </div>
+        <div class="trip-filters__filter">
+          <input id="filter-present" type="radio" name="trip-filter">
+          <label for="filter-present">Present</label>
+        </div>
+        <div class="trip-filters__filter">
+          <input id="filter-past" type="radio" name="trip-filter">
+          <label for="filter-past">Past</label>
+        </div>
+      </form>
+    `;
   }
+
+  render(container) {
+    container.insertAdjacentHTML('beforeend', this.getTemplate());
+  }
+}
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,42 +1,66 @@
 export default class PointView {
-    getTemplate() {
-      return `
+  constructor(point, destinations, offers) {
+    this.point = point;
+    this.destination = destinations.find((d) => d.id === point.destination);
+    this.offers = offers[point.type]?.filter((o) => point.offers.includes(o.id));
+  }
+
+  #formatDate(date) {
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  }
+
+  #getDuration(dateFrom, dateTo) {
+    const diff = dateTo - dateFrom;
+    const minutes = Math.floor(diff / 60000);
+    if (minutes < 60) {
+      return `${minutes}M`;
+    }
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours}H ${remainingMinutes}M`;
+  }
+
+  getTemplate() {
+    const {type, dateFrom, dateTo, basePrice} = this.point;
+
+    return `
+      <li class="trip-events__item">
         <div class="event">
           <div class="event__type">
-            <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+            <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
           </div>
-          <h3 class="event__title">Taxi Amsterdam</h3>
-  
+          <h3 class="event__title">${type} ${this.destination?.name || ''}</h3>
           <div class="event__schedule">
             <p class="event__time">
-              <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+              <time class="event__start-time">${this.#formatDate(dateFrom)}</time>
               &mdash;
-              <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+              <time class="event__end-time">${this.#formatDate(dateTo)}</time>
             </p>
-            <p class="event__duration">30M</p>
+            <p class="event__duration">${this.#getDuration(dateFrom, dateTo)}</p>
           </div>
-  
           <p class="event__price">
-            &euro;&nbsp;<span class="event__price-value">20</span>
+            &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
           </p>
-  
-          <h4 class="visually-hidden">Offers:</h4>
-          <ul class="event__selected-offers">
-            <li class="event__offer">
-              <span class="event__offer-title">Order Uber</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">20</span>
-            </li>
-          </ul>
-  
+          ${this.offers?.length ? `
+            <h4 class="visually-hidden">Offers:</h4>
+            <ul class="event__selected-offers">
+              ${this.offers.map((offer) => `
+                <li class="event__offer">
+                  <span class="event__offer-title">${offer.title}</span>
+                  &plus;&euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
+                </li>
+              `).join('')}
+            </ul>
+          ` : ''}
           <button class="event__rollup-btn" type="button">
             <span class="visually-hidden">Open event</span>
           </button>
         </div>
-      `;
-    }
-  
-    render(container) {
-      container.insertAdjacentHTML('beforeend', this.getTemplate());
-    }
+      </li>
+    `;
   }
+
+  render(container) {
+    container.insertAdjacentHTML('beforeend', this.getTemplate());
+  }
+}
