6c5c2b730aa3360aa58c5e8614666dd9bf856b62
IDDQD93 <ggteamsasha@gmail.com>
2025-05-23 23:13:59 +0000

    Реализована динамическая сортировка точек маршрута с обновлением списка

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,7 +1,7 @@
 import FiltersView from '../view/filters-view.js';
 import SortView from '../view/sort-view.js';
-import PointPresenter from './point-presenter.js';
 import Model from '../model/model.js';
+import PointPresenter from './point-presenter.js';
 import { SortType, FilterType } from '../const.js';
 
 export default class TripPresenter {
@@ -9,25 +9,24 @@ export default class TripPresenter {
     this.model = new Model();
 
     this.listContainer = null;
-    this.pointPresenters = new Map();
+    this.pointPresenters = new Map(); // для хранения PointPresenter по id
 
     this.currentSortType = SortType.DAY;
 
+    this.filtersComponent = null;
     this.sortComponent = null;
   }
 
   init() {
     this.renderList();
+    this.renderFilters();
     this.renderSort();
     this.renderPoints();
   }
 
-  renderList() {
-    const container = document.querySelector('.trip-events');
-    const listElement = document.createElement('ul');
-    listElement.classList.add('trip-events__list');
-    container.appendChild(listElement);
-    this.listContainer = listElement;
+  renderFilters() {
+    this.filtersComponent = new FiltersView();
+    document.querySelector('.trip-controls__filters').appendChild(this.filtersComponent.element);
   }
 
   renderSort() {
@@ -46,6 +45,14 @@ export default class TripPresenter {
     this.currentSortType = sortType;
     this.clearPoints();
     this.renderPoints();
+  }
+
+  renderList() {
+    const container = document.querySelector('.trip-events');
+    const listElement = document.createElement('ul');
+    listElement.classList.add('trip-events__list');
+    container.appendChild(listElement);
+    this.listContainer = listElement;
   }
 
   getSortedPoints() {
@@ -68,9 +75,21 @@ export default class TripPresenter {
 
   renderPoints() {
     const points = this.getSortedPoints();
-    points.forEach(point => {
-      const presenter = new PointPresenter(point, this.listContainer);
-      presenter.init();
+    const destinations = this.model.getDestinations();
+    const offers = this.model.getOffers();
+
+    // Удаляем предыдущие презентеры и очищаем контейнер
+    this.pointPresenters.forEach((presenter) => presenter.destroy());
+    this.pointPresenters.clear();
+    this.listContainer.innerHTML = '';
+
+    points.forEach((point) => {
+      const presenter = new PointPresenter(
+        this.listContainer,
+        this.handleDataChange.bind(this),
+        this.resetAllForms.bind(this)
+      );
+      presenter.init(point, destinations, offers);
       this.pointPresenters.set(point.id, presenter);
     });
   }
@@ -78,5 +97,18 @@ export default class TripPresenter {
   clearPoints() {
     this.pointPresenters.forEach(presenter => presenter.destroy());
     this.pointPresenters.clear();
+    if(this.listContainer) {
+      this.listContainer.innerHTML = '';
+    }
+  }
+
+  handleDataChange(updatedPoint) {
+    // Обновляем модель — здесь можно добавить вызов API и обновление модели
+    this.model.updatePoint(updatedPoint);
+    this.renderPoints();
+  }
+
+  resetAllForms() {
+    this.pointPresenters.forEach((presenter) => presenter.resetView());
   }
 }
