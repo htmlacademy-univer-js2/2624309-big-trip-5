d10ae557f114aea90c10158f3dde4397edfbc5ff
IDDQD93 <ggteamsasha@gmail.com>
2025-05-23 22:54:55 +0000

    Добавлен компонент сортировки и реализована логика сортировки точек маршрута в TripPresenter

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -3,12 +3,27 @@ import SortView from '../view/sort-view.js';
 import PointView from '../view/point-view.js';
 import EditPointView from '../view/form-edit-view.js';
 import Model from '../model/model.js';
+import PointPresenter from './point-presenter.js';
+import { SortType } from '../const.js';
+
+const sortFunctions = {
+  [SortType.DAY]: (points) => points.slice().sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom)),
+  [SortType.PRICE]: (points) => points.slice().sort((a, b) => b.basePrice - a.basePrice),
+  [SortType.TIME]: (points) => points.slice().sort((a, b) => {
+    const durationA = new Date(a.dateTo) - new Date(a.dateFrom);
+    const durationB = new Date(b.dateTo) - new Date(b.dateFrom);
+    return durationB - durationA;
+  }),
+};
 
 export default class TripPresenter {
   constructor() {
     this.model = new Model();
+
     this.listContainer = null;
-    this.components = [];
+    this.pointPresenters = new Map();
+
+    this.sortComponent = null;
   }
 
   init() {
@@ -24,8 +39,9 @@ export default class TripPresenter {
   }
 
   renderSort() {
-    const sortView = new SortView();
-    document.querySelector('.trip-events').appendChild(sortView.element);
+    this.sortComponent = new SortView();
+    document.querySelector('.trip-events').appendChild(this.sortComponent.element);
+    this.sortComponent.setSortTypeChangeHandler(this.handleSortTypeChange.bind(this));
   }
 
   renderList() {
@@ -36,50 +52,46 @@ export default class TripPresenter {
     this.listContainer = listElement;
   }
 
+  clearPoints() {
+    this.pointPresenters.forEach((presenter) => presenter.destroy());
+    this.pointPresenters.clear();
+    this.listContainer.innerHTML = '';
+  }
+
   renderPoints() {
-    const points = this.model.getPoints();
-    const destinations = this.model.getDestinations();
-    const offers = this.model.getOffers();
+    this.clearPoints();
+
+    let points = this.model.getPoints();
+    const sortType = this.model.getSort() || SortType.DAY;
+
+    if (!sortFunctions[sortType]) {
+      points = this.model.getPoints(); // fallback без сортировки
+    } else {
+      points = sortFunctions[sortType](points);
+    }
 
     points.forEach((point) => {
-      const pointView = new PointView(point, destinations, offers);
-      const editPointView = new EditPointView(point, destinations, offers);
-
-      this.listContainer.appendChild(pointView.element);
-
-      this.components.push({ pointView, editPointView });
-
-      pointView.expandButton.addEventListener('click', () => {
-        this.replacePointToForm(pointView, editPointView);
-      });
-
-      editPointView.formElement.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-        this.replaceFormToPoint(pointView, editPointView);
-      });
-
-      editPointView.collapseButton.addEventListener('click', () => {
-        this.replaceFormToPoint(pointView, editPointView);
-      });
-    });
-
-    document.addEventListener('keydown', (evt) => {
-      if (evt.key === 'Escape' || evt.key === 'Esc') {
-        this.components.forEach(({ pointView, editPointView }) => {
-          if (this.listContainer.contains(editPointView.element)) {
-            this.replaceFormToPoint(pointView, editPointView);
-          }
-        });
-      }
+      const pointPresenter = new PointPresenter(this.listContainer, this.handleDataChange.bind(this), this.handleViewChange.bind(this));
+      pointPresenter.init(point, this.model.getDestinations(), this.model.getOffers());
+      this.pointPresenters.set(point.id, pointPresenter);
     });
   }
 
-  replacePointToForm(pointView, editPointView) {
-    this.listContainer.replaceChild(editPointView.element, pointView.element);
+  handleSortTypeChange(sortType) {
+    if (sortType === this.model.getSort()) {
+      return; // сортировка не изменилась
+    }
+
+    this.model.setSort(sortType);
+    this.renderPoints();
   }
 
-  replaceFormToPoint(pointView, editPointView) {
-    this.listContainer.replaceChild(pointView.element, editPointView.element);
+  handleDataChange(updatedPoint) {
+    this.model.updatePoint(updatedPoint);
+    this.renderPoints();
   }
-  
+
+  handleViewChange() {
+    this.pointPresenters.forEach((presenter) => presenter.resetView());
+  }
 }
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,22 +1,36 @@
 import AbstractView from '../framework/view/abstract-view.js';
-export default class SortView {
-    get template() {
-      return `
-        <div class="trip-sort">
-          <div class="trip-sort__item">
-            <input id="sort-day" type="radio" name="trip-sort" checked>
-            <label for="sort-day">Day</label>
-          </div>
-          <div class="trip-sort__item">
-            <input id="sort-price" type="radio" name="trip-sort">
-            <label for="sort-price">Price</label>
-          </div>
-        </div>
-      `;
-    }
-  
-    render(container) {
-      container.insertAdjacentHTML('beforeend', this.getTemplate());
-    }
+
+const createSortTemplate = () => `
+  <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+    <div class="trip-sort__item trip-sort__item--day" data-sort-type="day">
+      <input id="sort-day" class="trip-sort__input visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+      <label class="trip-sort__btn" for="sort-day">Day</label>
+    </div>
+
+    <div class="trip-sort__item trip-sort__item--price" data-sort-type="price">
+      <input id="sort-price" class="trip-sort__input visually-hidden" type="radio" name="trip-sort" value="sort-price">
+      <label class="trip-sort__btn" for="sort-price">Price</label>
+    </div>
+
+    <div class="trip-sort__item trip-sort__item--time" data-sort-type="time">
+      <input id="sort-time" class="trip-sort__input visually-hidden" type="radio" name="trip-sort" value="sort-time">
+      <label class="trip-sort__btn" for="sort-time">Time</label>
+    </div>
+  </form>
+`;
+
+export default class SortView extends AbstractView {
+  get template() {
+    return createSortTemplate();
   }
-  
+
+  setSortTypeChangeHandler(callback) {
+    this.element.addEventListener('click', (evt) => {
+      const sortType = evt.target.closest('.trip-sort__item')?.dataset.sortType;
+      if (!sortType) {
+        return;
+      }
+      callback(sortType);
+    });
+  }
+}
